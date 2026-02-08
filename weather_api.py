bcf6e64d3cc2b9bd3a18cfe3aecf5130cbbed7d3
Serafim <serafim.sharkov@gmail.com>
2026-02-08 11:19:23 -0800

    trying to remember what I was doing when I last worked on this project

--- weather_api.py
+++ weather_api.py
@@ -3,13 +3,14 @@ import os
 from dotenv import load_dotenv
 import redis
 import json
+from datetime import date
 
 
 
 load_dotenv()
 API_KEY = os.getenv("WEATHER_API_KEY")
 
-LOCATION = 'San Francisco,CA'
+LOCATION = 'Kortum Trail, Jenner, CA 95450'
 BASE_URL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
 UNIT_GROUP = 'metric'
 CONTENT_TYPE = 'json'
@@ -25,19 +26,22 @@ base_url = f"{BASE_URL}{LOCATION}?unitGroup={UNIT_GROUP}&contentType={CONTENT_TY
 def main():
     
 
-    cached_weather_data = fetch_weather_data(LOCATION)
+    #cached_weather_data = fetch_weather_data(LOCATION)
 
     
 
     # Fetch user data directly from the external route
-    response = requests.get(BASE_URL)
+    response = requests.get(base_url)
     external_weather_data = response.json()
+    print(json.dumps(external_weather_data, indent=4, sort_keys=True))
+    # todays_weather = external_weather_data['datetime']
+    # print(todays_weather)
 
     # Compare the data
-    if cached_weather_data == external_weather_data:
-        print("Data retrieved from cache matches data fetched from the external route.")
-    else:
-        print("Data mismatch: Cached data differs from data fetched from the external route.")
+    # if cached_weather_data == external_weather_data:
+    #     print("Data retrieved from cache matches data fetched from the external route.")
+    # else:
+    #     print("Data mismatch: Cached data differs from data fetched from the external route.")
 
 
 
@@ -48,50 +52,50 @@ def main():
 
 
 # Function to fetch weather data either from the cache or the external route
-def fetch_weather_data(location):
-    r = redis.Redis(host='localhost', port=6379, db=0)
+# def fetch_weather_data(location):
+#     r = redis.Redis(host='localhost', port=6379, db=0)
 
-    # Check if data is available in the cache
-    weather_data = r.get(location)
-    if weather_data is None:
-        try:
-          response = requests.get(base_url, timeout=5, verify=True)
-          response.raise_for_status() 
+#     # Check if data is available in the cache
+#     weather_data = r.get(location)
+#     if weather_data is None:
+#         try:
+#           response = requests.get(base_url, timeout=5, verify=True)
+#           response.raise_for_status() 
 
-        except requests.exceptions.HTTPError as errh:
-         print(f"HTTP Error: {errh}")
-        except requests.exceptions.ReadTimeout as errrt:
-            print("Timeout Error: The server took too long to respond.")
-        except requests.exceptions.ConnectionError as conerr:
-            print("Connection Error: Check your internet or the URL.")
-        except requests.exceptions.RequestException as errex:
-            print(f"General Error: {errex}")
+#         except requests.exceptions.HTTPError as errh:
+#          print(f"HTTP Error: {errh}")
+#         except requests.exceptions.ReadTimeout as errrt:
+#             print("Timeout Error: The server took too long to respond.")
+#         except requests.exceptions.ConnectionError as conerr:
+#             print("Connection Error: Check your internet or the URL.")
+#         except requests.exceptions.RequestException as errex:
+#             print(f"General Error: {errex}")
 
-        else:
-            print("Data was retrieved!")
+#         else:
+#             print("Data was retrieved!")
     
 
-        try:
-            weather_data = response.json()
+#         try:
+#             weather_data = response.json()
         
-            if 'currentConditions' in weather_data:
-                current_conditions = weather_data['currentConditions']
+#             if 'currentConditions' in weather_data:
+#                 current_conditions = weather_data['currentConditions']
             
-                condition_desc = current_conditions.get('conditions', 'Unknown')
-                condition_time = current_conditions.get('datetime', 'Unknown')
+#                 condition_desc = current_conditions.get('conditions', 'Unknown')
+#                 condition_time = current_conditions.get('datetime', 'Unknown')
             
-                print(f"Condition: {condition_desc}")
-                print(f"Time: {condition_time}")
+#                 print(f"Condition: {condition_desc}")
+#                 print(f"Time: {condition_time}")
 
-                r.set(location, json.dumps(weather_data))
-            else:
-                print("Current conditions data not found in response.")
+#                 r.set(location, json.dumps(weather_data))
+#             else:
+#                 print("Current conditions data not found in response.")
             
-        except ValueError:
-            print("Error: content is not valid JSON")        
+#         except ValueError:
+#             print("Error: content is not valid JSON")        
 
 
-    return weather_data
+#     return weather_data
 
 
 
